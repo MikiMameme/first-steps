f45d9123f3ba1cfc7d6b325a6d796365b3f599db
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-05-31 23:52:13 +0900

    学習成果１（電卓プログラム）
    初歩の学習成果の確認のため簡単な電卓プログラムを書いてはどうかとChatGPTに提案されたため作りました。
    課題として、乗算除算を追加する課題をこなしました。

---
+++ 学習成果１（電卓プログラム）/main.c
@@ -0,0 +1,18 @@
+/* 電卓プログラム　ChatGPTより　乗算除算ができるように改良しました */
+#include <stdio.h>
+
+void calc(int a,int b,char op){
+	if(op == '+') printf("%d\n",a + b);
+	else if(op == '-') printf("%d\n",a - b);
+	else if(op == '*') printf("%d\n",a * b); /* 乗算ができるように改良しました */
+	else if(op == '/') printf("%d\n",a / b); /* 除算ができるように改良しました */
+}
+
+int main(){
+	int a,b;
+	char op;
+	printf("数字　演算子　数字　の順で入力(注意：小数点はできません)　例：1 + 1 ： "); /* 説明文をわかりやすく改良しました */
+	scanf("%d %c %d", &a, &op, &b);
+	calc(a, b, op);
+	return 0;
+}
