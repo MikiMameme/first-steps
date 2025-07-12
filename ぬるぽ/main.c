2e4f2c4050d850adbc54f8d6ca459722d41f1209
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-12 20:51:39 +0900

    ぬるぽ
    C言語では「ぬるぽ」は危険であることを学習しました。
    （NULLポインタ参照の瞬間にプログラムが停止します。OSの保護機能が効いてることも確認しました。）

---
+++ ぬるぽ/main.c
@@ -0,0 +1,21 @@
+/* 教訓：Cでぬるぽは危険 */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void segfault_handler(int sig) 
+{
+    printf("ガッ！\n");
+    exit(1);
+}
+
+int main(void) 
+{
+    signal(SIGSEGV, segfault_handler);
+    
+    int *p = NULL;
+    *p = 42;			/* ぬるぽ発生！セグメンテーション違反 */
+    
+    return 0;
+}
